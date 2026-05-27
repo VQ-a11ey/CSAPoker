69ed763db8b1b4ab5cc3c4ba0ff401fe716f5f5f
VQ-a11ey <vickyqin2010@gmail.com>
2026-05-27 16:20:02 -0700

    added javadocs to card

--- Card.java
+++ Card.java
@@ -1,16 +1,33 @@
+package com.example;
+
+/**
+ * @author Vicky Qin
+ * @date 5/27/2026
+ * Classifies cards by rank, suit, and name. 
+ */
 public class Card implements Comparable<Object> {
     private int rank;
     private String suit;
-
-    public Card(int i, String s){
-        rank = i;
-        suit = s;
+/**
+ * Creates a Card Object with inputted values of Rank rank and Suit suit
+ * @param rank
+ * @param suit
+ */
+    public Card(int rank, String suit){
+        this.rank = rank;
+        this.suit = suit;
     }
-
+/**
+ * Returns the rank of a Card.
+ * @return rank
+ */
     public int getRank(){
         return rank;
     }
-
+/**
+ * Returns the name of a Card.
+ * @return rank of suit
+ */
     public String getName(){
         if (rank <= 10)
             return "" + rank + " of " + suit;
@@ -22,12 +39,18 @@ public class Card implements Comparable<Object> {
             return "King of " + suit;
         return "Ace of " + suit;
     }
-
+/**
+ * Returns the suit of a Card.
+ * @return suit;
+ */
     public String getSuit(){
         return suit;
     }
     
-    
+/**
+ * Compares cards by rank.
+ * @return Rank1-Rank2
+ */
     public int compareTo(Object obj){
         return rank-((Card)obj).getRank();
     }
