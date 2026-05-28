bcb06b3a88cdbbeb8dd92b9284588fbb7a7cc8a8
Sophia Fan <88063440+sophiaxfan@users.noreply.github.com>
2026-05-28 11:22:24 -0700

    terminal version

--- Deck.java
+++ Deck.java
@@ -1,30 +1,24 @@
-package com.example;
 import java.util.ArrayList;
 
-/**
- * @author Anna Chen
- * @date 5/27/2026
- */
 public class Deck {
     private ArrayList<Card> cards;
 
-    /**
-     * Creates a new deck of 52 cards, with ranks 2-14 (where 11-14 are Jack, Queen, King, Ace) 
-     * Suits Hearts, Spades, Diamonds, Clubs
-     */
     public Deck() {
         cards = new ArrayList<Card>(52);
-        for (int i = 2; i < 15; i++) {
+        for (int i = 1; i < 14; i++) {
             cards.add(new Card(i, "Hearts"));
+        }
+        for (int i = 1; i < 14; i++) {
             cards.add(new Card(i, "Spades"));
+        }
+        for (int i = 1; i < 14; i++) {
             cards.add(new Card(i, "Diamonds"));
+        }
+        for (int i = 1; i < 14; i++) {
             cards.add(new Card(i, "Clubs"));
         }
     }
 
-    /**
-     * @return a random card from the deck, and removes it from the deck
-     */
     public Card chooseCard() {
         Card c = cards.remove((int) (Math.random() * cards.size()));
         return c;
