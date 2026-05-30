9ad0c1bfd6c69a5d476185c2b8ba8d3327fb3e1c
Sophia Fan <88063440+sophiaxfan@users.noreply.github.com>
2026-05-29 20:55:50 -0700

    Enhance Card class with Javadoc comments
    Added Javadoc comments for constructors and methods.

--- Card.java
+++ Card.java
@@ -1,17 +1,34 @@
 package com.example;
+/**
+ * @author Vicky Qin
+ * @date 5/27/2026
+ * The Card class represents a card in the poker game. It contains information about the card's rank and suit, and implements the Comparable interface to allow for comparison between cards based on their rank.
+ */
 public class Card implements Comparable<Object> {
     private int rank;
     private String suit;
 
-    public Card(int i, String s){
-        rank = i;
-        suit = s;
+    /**
+     * Creates a Card Object with inputted values of Rank rank and Suit suit
+     * @param rank
+     * @param suit
+     */
+    public Card(int rank, String suit){
+        this.rank = rank;
+        this.suit = suit;
     }
-
+/**
+ * Returns the rank of a Card.
+ * @return rank
+ */
     public int getRank(){
         return rank;
     }
 
+/**
+ * Returns the name of a Card.
+ * @return rank of suit (e.g. "Jack of Hearts")
+ */
     public String getName(){
         if (rank <= 10)
             return "" + rank + " of " + suit;
@@ -24,11 +41,19 @@ public class Card implements Comparable<Object> {
         return "Ace of " + suit;
     }
 
+    /**
+     * Returns the suit of a Card.
+     * @return suit
+     */
     public String getSuit(){
         return suit;
     }
     
-    
+    /**
+     * Compares this card to another card based on their ranks.
+     * @param obj the object to compare to
+     * @return a negative integer, zero, or a positive integer as this card is less than, equal to, or greater than the specified object
+     */
     public int compareTo(Object obj){
         return rank-((Card)obj).getRank();
     }
